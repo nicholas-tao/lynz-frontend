6cd0c3884303d69dac4f7d30771cfd6650d58d68
nicholas-tao <tao.nicholas1@gmail.com>
2020-04-21 14:43:09 -0400

    add dropdown for busyness level submission

--- src/components/Submitpage.component.js
+++ src/components/Submitpage.component.js
@@ -1,11 +1,31 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 import "./Components.css";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 
-export default class Submitpage extends Component {
+const Submitpage = (props) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  render() {
-    return (
-      <p> hi! this is the submitpage! </p>
-    );
-  }
-}
+  const toggle = () => setDropdownOpen((prevState) => !prevState);
+
+  return (
+    <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+      <DropdownToggle caret>Select Busyness</DropdownToggle>
+      <DropdownMenu>
+        <DropdownItem>Not Busy</DropdownItem>
+        <DropdownItem>A Little Busy</DropdownItem>
+        <DropdownItem>Busy</DropdownItem>
+        <DropdownItem>Very Busy</DropdownItem>
+        <DropdownItem>Short Lineup to Get In</DropdownItem>
+        <DropdownItem>Long Lineup to Get In</DropdownItem>
+        <DropdownItem>Crazy (Do not come)</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
+
+export default Submitpage;
