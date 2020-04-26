cf7d765dd49900fba96f8e9f3a5bba454e2896a3
nicholas-tao <tao.nicholas1@gmail.com>
2020-04-26 19:50:25 -0400

    use hashrouter instead

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
 /*import "bootstrap/dist/css/bootstrap.min.css"; */
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route, withRouter } from "react-router-dom";
 import NavbarComp from "./components/NavbarComp.component";
 import Homepage from "./components/Homepage.component";
 import Viewpage from "./components/Viewpage.component";
 import Submitpage from "./components/Submitpage.component";
 import "./App.css";
 
-function App() {
-  return (
-    <Router>
-      <div className="container">
-        <NavbarComp />
-        <br />
-        <Route path="/" exact component={Homepage} />
-        <Route path="/busyness" component={Viewpage} />
-        <Route path="/submit" component={Submitpage} />
-      </div>
-    </Router>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <NavbarComp />
+          <Route
+            path={process.env.PUBLIC_URL}
+            exact
+            component={withRouter(Homepage)}
+          />
+          <Route path={"/busyness"} exact component={withRouter(Viewpage)} />
+          <Route path={"/submit"} exact component={withRouter(Submitpage)} />
+        </div>
+      </Router>
+    );
+  }
 }
-export default App;
