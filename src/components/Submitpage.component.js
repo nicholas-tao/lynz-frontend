3f13cbba10db765163cafd6718fca62852805160
nicholas-tao <tao.nicholas1@gmail.com>
2020-04-26 00:24:38 -0400

    puhsing for adam to see

--- src/components/Submitpage.component.js
+++ src/components/Submitpage.component.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import PlacesAutocomplete from 'react-places-autocomplete';
+import PlacesAutocomplete from "react-places-autocomplete";
 import {
   ButtonDropdown,
   DropdownMenu,
@@ -8,7 +8,7 @@ import {
   DropdownToggle,
 } from "reactstrap";
 import "./Components.css";
-import helping from "..//images/helpingedit.jpg"
+import helping from "..//images/helpingedit.jpg";
 
 class Submitpage extends React.Component {
   constructor(props) {
@@ -16,98 +16,94 @@ class Submitpage extends React.Component {
     this.toggle = this.toggle.bind(this);
     this.select = this.select.bind(this);
 
-
     this.handleMouseHover1 = this.handleMouseHover1.bind(this);
     this.handleMouseHover2 = this.handleMouseHover2.bind(this);
     this.handleMouseHover3 = this.handleMouseHover3.bind(this);
-    this.handleMouseHover4 = this.handleMouseHover4.bind(this);   //LOLLL
+    this.handleMouseHover4 = this.handleMouseHover4.bind(this); //LOLLL
     this.handleMouseHover5 = this.handleMouseHover5.bind(this);
     this.handleMouseHover6 = this.handleMouseHover6.bind(this);
-
 
     this.onSubmit = this.onSubmit.bind(this);
     this.state = {
       dropdownOpen: false,
       busyness: "Select Busyness",
-      latitude:"",
-      longitude:"",
-      address:"",
-      isHovering1:false,
-      isHovering2:false,
-      isHovering3:false,    //for each of the buttons LOL
-      isHovering4:false,
-      isHovering5:false,
-      isHovering6:false
+      latitude: "",
+      longitude: "",
+      address: "",
+      isHovering1: false,
+      isHovering2: false,
+      isHovering3: false, //for each of the buttons LOL
+      isHovering4: false,
+      isHovering5: false,
+      isHovering6: false,
     };
   }
-//autocomplete stuff
-  handleChange = address => {
+  //autocomplete stuff
+  handleChange = (address) => {
     this.setState({ address });
   };
 
-//hovering for each div
-handleMouseHover1() {
-  this.setState(this.toggleHoverState1);
-}
+  //hovering for each div
+  handleMouseHover1() {
+    this.setState(this.toggleHoverState1);
+  }
 
-handleMouseHover2() {
-  this.setState(this.toggleHoverState2);
-}
+  handleMouseHover2() {
+    this.setState(this.toggleHoverState2);
+  }
 
-handleMouseHover3() {
-  this.setState(this.toggleHoverState3);
-}
+  handleMouseHover3() {
+    this.setState(this.toggleHoverState3);
+  }
 
-handleMouseHover4() {
-  this.setState(this.toggleHoverState4);
-}
+  handleMouseHover4() {
+    this.setState(this.toggleHoverState4);
+  }
 
-handleMouseHover5() {
-  this.setState(this.toggleHoverState5);
-}
+  handleMouseHover5() {
+    this.setState(this.toggleHoverState5);
+  }
 
-handleMouseHover6() {
-  this.setState(this.toggleHoverState6);
-}
+  handleMouseHover6() {
+    this.setState(this.toggleHoverState6);
+  }
 
+  toggleHoverState1(state) {
+    return {
+      isHovering1: !state.isHovering1,
+    };
+  }
 
+  toggleHoverState2(state) {
+    return {
+      isHovering2: !state.isHovering2,
+    };
+  }
 
-toggleHoverState1(state) {
-  return {
-    isHovering1: !state.isHovering1,
-  };
-}
+  toggleHoverState3(state) {
+    return {
+      isHovering3: !state.isHovering3,
+    };
+  }
 
-toggleHoverState2(state) {
-  return {
-    isHovering2: !state.isHovering2,
-  };
-}
+  toggleHoverState4(state) {
+    return {
+      isHovering4: !state.isHovering4,
+    };
+  }
+  toggleHoverState5(state) {
+    return {
+      isHovering5: !state.isHovering5,
+    };
+  }
 
-toggleHoverState3(state) {
-  return {
-    isHovering3: !state.isHovering3,
-  };
-}
+  toggleHoverState6(state) {
+    return {
+      isHovering6: !state.isHovering6,
+    };
+  }
 
-toggleHoverState4(state) {
-  return {
-    isHovering4: !state.isHovering4,
-  };
-}
-toggleHoverState5(state) {
-  return {
-    isHovering5: !state.isHovering5,
-  };
-}
-
-toggleHoverState6(state) {
-  return {
-    isHovering6: !state.isHovering6,
-  };
-}
-
-//get location
+  //get location
   // componentDidMount() {
   //   navigator.geolocation.getCurrentPosition(
   //     function (position) {
@@ -121,7 +117,6 @@ toggleHoverState6(state) {
   //   );
   // }
 
- 
   toggle() {
     this.setState({
       dropdownOpen: !this.state.dropdownOpen,
@@ -136,180 +131,240 @@ toggleHoverState6(state) {
     });
   }
 
-
   onSubmit(e) {
     e.preventDefault(); //idk what this does; maybe prevents empty fields from being sent
 
-    const busyness = {
-      address: this.state.address,
+    const busynessToSend = {
+      storeAddress: this.state.address,
       busyness: this.state.busyness,
-     // latitude: this.state.latitude,
-    //  longitude: this.state.longitude
+      // latitude: this.state.latitude,
+      //  longitude: this.state.longitude
     };
 
-    console.log(busyness)
-    /*  axios
-      .post("http://localhost:5000/busyness/add", busyness)
-      .then((res) => console.log(res.data));*/
+    axios
+      .post("http://localhost:5000/busyness/add", busynessToSend)
+      .then((res) => console.log(res.data));
 
     this.setState({
       busyness: "Select Busyness",
       address: "",
     });
   }
-  
 
   render() {
-    
     return (
-      <div className = "allsubmit">
+      <div className="allsubmit">
         <div className="busyness">
-          <div className = "location">
-            <PlacesAutocomplete 
+          <div className="location">
+            <PlacesAutocomplete
               value={this.state.address}
               onChange={this.handleChange}
               onSelect={this.handleSelect}
-              debounce = {500}
+              debounce={500}
             >
-            {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-              <div className = "location4">
-                <input className = "location5"
-                  {...getInputProps({
-                    placeholder: 'Enter City and Store',
-                  })}
-                />
-                <div className="location2">
-                  {loading && <div></div>}
-                  {suggestions.map(suggestion => {
-                    const className = suggestion.active
-                      ? 'suggestion-item--active'  
-                      : 'suggestion-item';
-                    // inline style for demonstration purpose
-                    const style = suggestion.active
-                      ? { backgroundColor: '#b8b2a3', cursor: 'pointer' }
-                      : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                    return (
-                      <div className = "location3"
-                        {...getSuggestionItemProps(suggestion, {
-                          className,
-                          style,
-                        })}
-                      >
-                        <span>{    suggestion.description    }</span>
-                      </div>
-                    );
-                  })}
+              {({
+                getInputProps,
+                suggestions,
+                getSuggestionItemProps,
+                loading,
+              }) => (
+                <div className="location4">
+                  <input
+                    className="location5"
+                    {...getInputProps({
+                      placeholder: "Enter City and Store",
+                    })}
+                  />
+                  <div className="location2">
+                    {loading && <div></div>}
+                    {suggestions.map((suggestion) => {
+                      const className = suggestion.active
+                        ? "suggestion-item--active"
+                        : "suggestion-item";
+                      // inline style for demonstration purpose
+                      const style = suggestion.active
+                        ? { backgroundColor: "#b8b2a3", cursor: "pointer" }
+                        : { backgroundColor: "#ffffff", cursor: "pointer" };
+                      return (
+                        <div
+                          className="location3"
+                          {...getSuggestionItemProps(suggestion, {
+                            className,
+                            style,
+                          })}
+                        >
+                          <span>{suggestion.description}</span>
+                        </div>
+                      );
+                    })}
+                  </div>
                 </div>
-              </div>
-            )}
+              )}
             </PlacesAutocomplete>
           </div>
 
           <div className="submitToggle">
-            <ButtonDropdown className = "submitter"
+            <ButtonDropdown
+              className="submitter"
               isOpen={this.state.dropdownOpen}
               toggle={this.toggle}
             >
               <DropdownToggle>{this.state.busyness}</DropdownToggle>
               <DropdownMenu>
-                
-                <DropdownItem >
-                  <div
-                  onClick={(e) => { this.select(e); this.handleMouseHover1();}}
-                  onMouseEnter={this.handleMouseHover1}
-                  onMouseLeave={this.handleMouseHover1}
-                  > Not Busy </div>
-                </DropdownItem>  {this.state.isHovering1 &&
-                      <div className = "hover1">
-                        <u><i>(No lineup to get in, short checkout lines)</i></u>     
-                      </div>}
-                  
                 <DropdownItem>
                   <div
-                  onClick={(e) => { this.select(e); this.handleMouseHover2();}}
-                  onMouseEnter={this.handleMouseHover2}
-                  onMouseLeave={this.handleMouseHover2}
-                  > Somewhat Busy </div>
-                </DropdownItem>  {this.state.isHovering2 &&
-                      <div className = "hover1">
-                        <u><i>(No lineup to get in, moderate checkout lines)</i></u>     
-                      </div>}
-
+                    onClick={(e) => {
+                      this.select(e);
+                      this.handleMouseHover1();
+                    }}
+                    onMouseEnter={this.handleMouseHover1}
+                    onMouseLeave={this.handleMouseHover1}
+                  >
+                    {" "}
+                    Not Busy{" "}
+                  </div>
+                </DropdownItem>{" "}
+                {this.state.isHovering1 && (
+                  <div className="hover1">
+                    <u>
+                      <i>(No lineup to get in, short checkout lines)</i>
+                    </u>
+                  </div>
+                )}
                 <DropdownItem>
                   <div
-                  onClick={(e) => { this.select(e); this.handleMouseHover3();}}
-                  onMouseEnter={this.handleMouseHover3}
-                  onMouseLeave={this.handleMouseHover3}
-                  > Moderately Busy </div>
-                </DropdownItem>  {this.state.isHovering3 &&
-                      <div className = "hover1">
-                        <u><i>(No lineup to get in, long checkout lines)</i></u>     
-                      </div>}
-                
+                    onClick={(e) => {
+                      this.select(e);
+                      this.handleMouseHover2();
+                    }}
+                    onMouseEnter={this.handleMouseHover2}
+                    onMouseLeave={this.handleMouseHover2}
+                  >
+                    {" "}
+                    Somewhat Busy{" "}
+                  </div>
+                </DropdownItem>{" "}
+                {this.state.isHovering2 && (
+                  <div className="hover1">
+                    <u>
+                      <i>(No lineup to get in, moderate checkout lines)</i>
+                    </u>
+                  </div>
+                )}
                 <DropdownItem>
                   <div
-                  onClick={(e) => { this.select(e); this.handleMouseHover4();}}
-                  onMouseEnter={this.handleMouseHover4}
-                  onMouseLeave={this.handleMouseHover4}
-                  > Busy </div>
-                </DropdownItem>  {this.state.isHovering4 &&
-                      <div className = "hover1">
-                        <u><i>(Short lineup to get in, less than 10 min)</i></u>     
-                      </div>}
-                
+                    onClick={(e) => {
+                      this.select(e);
+                      this.handleMouseHover3();
+                    }}
+                    onMouseEnter={this.handleMouseHover3}
+                    onMouseLeave={this.handleMouseHover3}
+                  >
+                    {" "}
+                    Moderately Busy{" "}
+                  </div>
+                </DropdownItem>{" "}
+                {this.state.isHovering3 && (
+                  <div className="hover1">
+                    <u>
+                      <i>(No lineup to get in, long checkout lines)</i>
+                    </u>
+                  </div>
+                )}
                 <DropdownItem>
                   <div
-                  onClick={(e) => { this.select(e); this.handleMouseHover5();}}
-                  onMouseEnter={this.handleMouseHover5}
-                  onMouseLeave={this.handleMouseHover5}
-                  > Very Busy </div>
-                </DropdownItem>  {this.state.isHovering5 &&
-                      <div className = "hover1">
-                        <u><i>(Long lineup to get in, less than 30 min)</i></u>     
-                      </div>}
-
+                    onClick={(e) => {
+                      this.select(e);
+                      this.handleMouseHover4();
+                    }}
+                    onMouseEnter={this.handleMouseHover4}
+                    onMouseLeave={this.handleMouseHover4}
+                  >
+                    {" "}
+                    Busy{" "}
+                  </div>
+                </DropdownItem>{" "}
+                {this.state.isHovering4 && (
+                  <div className="hover1">
+                    <u>
+                      <i>(Short lineup to get in, less than 10 min)</i>
+                    </u>
+                  </div>
+                )}
                 <DropdownItem>
                   <div
-                  onClick={(e) => { this.select(e); this.handleMouseHover6();}}
-                  onMouseEnter={this.handleMouseHover6}
-                  onMouseLeave={this.handleMouseHover6}
-                  > Extremely Busy </div>
-                </DropdownItem>  {this.state.isHovering6 &&
-                      <div className = "hover1">
-                        <u><i>(Very long lineup to get in, more than 30 min)</i></u>     
-                      </div>}
-
+                    onClick={(e) => {
+                      this.select(e);
+                      this.handleMouseHover5();
+                    }}
+                    onMouseEnter={this.handleMouseHover5}
+                    onMouseLeave={this.handleMouseHover5}
+                  >
+                    {" "}
+                    Very Busy{" "}
+                  </div>
+                </DropdownItem>{" "}
+                {this.state.isHovering5 && (
+                  <div className="hover1">
+                    <u>
+                      <i>(Long lineup to get in, less than 30 min)</i>
+                    </u>
+                  </div>
+                )}
+                <DropdownItem>
+                  <div
+                    onClick={(e) => {
+                      this.select(e);
+                      this.handleMouseHover6();
+                    }}
+                    onMouseEnter={this.handleMouseHover6}
+                    onMouseLeave={this.handleMouseHover6}
+                  >
+                    {" "}
+                    Extremely Busy{" "}
+                  </div>
+                </DropdownItem>{" "}
+                {this.state.isHovering6 && (
+                  <div className="hover1">
+                    <u>
+                      <i>(Very long lineup to get in, more than 30 min)</i>
+                    </u>
+                  </div>
+                )}
               </DropdownMenu>
             </ButtonDropdown>
           </div>
- 
-        
-          <div className="button2">
-            <button className = "actualButton2" onClick={(e) => this.onSubmit(e)}> Submit! </button>
-          </div>
-        
 
-      </div>
+          <div className="button2">
+            <button className="actualButton2" onClick={(e) => this.onSubmit(e)}>
+              {" "}
+              Submit!{" "}
+            </button>
+          </div>
+        </div>
         <div className="helpingpic">
-         <img id = "helping"
+          <img
+            id="helping"
             alt=""
-            src= {helping}
+            src={helping}
             width="1100px"
             height="500px"
           />
-      </div>
-
-        <div className = "footer">
-            <div className = "bodyleft">
-              <p className = "footertext1"> Created by Adam Lam, Matthew Jiao, Nicholas Tao</p>
-            </div>
-
-            <div className = "bodyright" >
-              <p className = "footertext2"> &copy;  Lynz 2020 </p>
-            </div>
         </div>
 
-      </div>  
+        <div className="footer">
+          <div className="bodyleft">
+            <p className="footertext1">
+              {" "}
+              Created by Adam Lam, Matthew Jiao, Nicholas Tao
+            </p>
+          </div>
+
+          <div className="bodyright">
+            <p className="footertext2"> &copy; Lynz 2020 </p>
+          </div>
+        </div>
+      </div>
     );
   }
 }
--- src/components/Viewpage.component.js
+++ src/components/Viewpage.component.js
@@ -1,8 +1,9 @@
 import React from "react";
 import axios from "axios";
 import "./Components.css";
-import test from "./test.json";
-var classNames = require('classnames');
+//import test from "./test.json";
+var test = [];
+var classNames = require("classnames");
 
 class Viewpage extends React.Component {
   constructor(props) {
@@ -18,18 +19,18 @@ class Viewpage extends React.Component {
       radius: "",
       storedata: "",
       received: false,
-      textClass: this.computeClass(0)
+      textClass: this.computeClass(0),
     };
   }
-  makeGrid(test){
+  makeGrid(test) {
     this.changeStoredata(test);
     console.log("hi");
   }
 
-  changeStoredata(test){
+  changeStoredata(test) {
     this.setState({
       storedata: test.data,
-      received: true
+      received: true,
     });
   }
 
@@ -53,6 +54,7 @@ class Viewpage extends React.Component {
   }
 
   onSubmit(e) {
+    test = [];
     e.preventDefault();
 
     const coordsAndRadius = {
@@ -60,25 +62,24 @@ class Viewpage extends React.Component {
       longitude: this.state.longitude,
       radius: this.state.radius,
     };
-    //uncomment!
-    /*  axios
+
+    axios
       .post("http://localhost:5000/busyness/view", coordsAndRadius)
       .then((res) => console.log(res.data));
-*/
 
-   /* axios
+    axios
       .get("http://localhost:5000/busyness/getstores")
       .then((response) => {
-        this.setState({ storedata: response.data }); //for nick the line assignment thats needed is at 153
+        //this.setState({ storedata: response.data }); //for nick the line assignment thats needed is at 153
         //see if you can store the object (not object.data) into somewhere then go to line 154
         console.log(response.data);
-        test = response; see line 154; 
+        test = response.data;
       })
       .catch((error) => {
         console.log(error);
-      });*/
+      });
     //pass the file into here as test
-    this.makeGrid(test); // im not sure if this method really does anything 
+    this.makeGrid(test); // im not sure if this method really does anything
 
     console.log(this.state);
     this.setState({
@@ -87,102 +88,97 @@ class Viewpage extends React.Component {
   }
 
   renderMatches(data) {
-    
-    if(this.state.received === true){
-    return data.map(data => {
-      
-      return (
+    if (this.state.received === true) {
+      console.log("true");
+      return data.map((data) => {
+        return (
           <div className="col-lg-4">
-              <div className="one">
-                <div className = "Store">
-                  <h3> {data.name}</h3>
-                </div>
-
-                  {this.computeClass(data.busyness)}
-                  
-                <div className = "Address">
-                  <p><i>{data.address}</i></p>
-                </div>
+            <div className="one">
+              <div className="Store">
+                <h3> {data.name}</h3>
               </div>
+
+              {this.computeClass(data.busyness)}
+
+              <div className="Address">
+                <p>
+                  <i>{data.address}</i>
+                </p>
+              </div>
+            </div>
           </div>
+        );
+      });
+    } else {
+      return (
+        <div className="greeting"> Enter a radius to see stores near you!</div>
       );
-  })
-}
-else{
-  return(
-    <div className = "greeting"> Hi! Enter a radius to see stores near you!</div>
-  )
-}
-}
-
-computeClass(e){
-  var btnClass = classNames({
-  
- 
-    "green": e == "Not Busy",
-    "green-yellow": e == "Somewhat Busy",
-    "yellow": e == "Moderately Busy",
-    "orange": e == "Busy",
-    "red": e == "Very Busy",
-    "brown": e == "Extremely Busy",
-    "blue": e == "Insufficient Data"
+    }
   }
-);
-  return (
-    <div className = {btnClass} >
-      <b>{e}</b>
-    </div>
-  );
-}
 
+  computeClass(e) {
+    var btnClass = classNames({
+      green: e == "Not Busy",
+      "green-yellow": e == "Somewhat Busy",
+      yellow: e == "Moderately Busy",
+      orange: e == "Busy",
+      red: e == "Very Busy",
+      brown: e == "Extremely Busy",
+      blue: e == "Insufficient Data",
+    });
+    return (
+      <div className={btnClass}>
+        <b>{e}</b>
+      </div>
+    );
+  }
 
   render() {
-    //think i need code here 
+    //think i need code here
     // let greet= "?"
-   
+
     // let listNames = "";
     // let listAddr = "";
     // let listBusy = "";
     // let number = "";
     // let table = "";
     // let gooddata = [];
-    let data = ""
-    if(this.state.received === true){
-    // greet = "hihi";
-    // listNames = data.map((d) => <li key={d.name}>{d.name}</li>);
-    // listAddr = data.map((d) => <li key={d.address}>{d.address}</li>);
-    // listBusy = data.map((d) => <li key={d.busyness}>{d.busyness}</li>); useless for now
-    // number = listNames.length;
+    let data = "";
+    if (this.state.received === true) {
+      // greet = "hihi";
+      // listNames = data.map((d) => <li key={d.name}>{d.name}</li>);
+      // listAddr = data.map((d) => <li key={d.address}>{d.address}</li>);
+      // listBusy = data.map((d) => <li key={d.busyness}>{d.busyness}</li>); useless for now
+      // number = listNames.length;
 
-    data = test.data;    //setting a variable (data) to the json.data 
-    //table = this.Table(listNames, listAddr, listBusy, number);
-    // for(let i = 0; i < number; i++){
-    //   gooddata[i] = listNames[i]+" "+ listAddr[i]+" "+ listBusy[i];
-    // }
-
-  }
+      data = test; //setting a variable (data) to the json.data
+      //table = this.Table(listNames, listAddr, listBusy, number);
+      // for(let i = 0; i < number; i++){
+      //   gooddata[i] = listNames[i]+" "+ listAddr[i]+" "+ listBusy[i];
+      // }
+    }
     return (
       <div className="all">
-        <div className = "content-wrap">
-        <div className="viewing">
-            <input id = "radius"
-              placeholder="Enter the Radius (km)"
+        <div className="content-wrap">
+          <div className="viewing">
+            <input
+              id="radius"
+              placeholder="Enter the Radius (m)"
               value={this.state.radius}
               onChange={this.onChangeRadius}
             />
-            <button 
-              className = "actualButton1" 
-              onClick={(e) => { this.onSubmit(e) }}> 
-              Get Stores! 
+            <button
+              className="actualButton1"
+              onClick={(e) => {
+                this.onSubmit(e);
+              }}
+            >
+              Get Stores!
             </button>
+          </div>
+
+          <div className="row">{this.renderMatches(data)}</div>
         </div>
-
-
-        <div className = "row">
-          {this.renderMatches(data)}
-            
-       </div>
-       </div>
 
         <div className="footerbottom">
           <div className="bodyleft">
@@ -193,7 +189,7 @@ computeClass(e){
           </div>
 
           <div className="bodyright">
-            <p className="footertext2"> &copy;  Lynz 2020 </p>
+            <p className="footertext2"> &copy; Lynz 2020 </p>
           </div>
         </div>
       </div>
