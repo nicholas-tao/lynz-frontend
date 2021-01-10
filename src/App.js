2d892d0848e5e3a1ea2dcc559d3ca19fb0978aa1
Nicholas <tao.nicholas1@gmail.com>
2021-01-10 17:25:13 -0500

    update npm packages (vulnerabilities fixed lol) and still tryna fix this

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React from "react";
 /*import "bootstrap/dist/css/bootstrap.min.css"; */
-import { HashRouter as Router, Route, withRouter } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import NavbarComp from "./components/NavbarComp.component";
 import Homepage from "./components/Homepage.component";
 import Viewpage from "./components/Viewpage.component";
 import Submitpage from "./components/Submitpage.component";
 import "./App.css";
+import { Container } from "reactstrap";
 
 export default class App extends React.Component {
   render() {
     return (
-      <Router>
-        <div className="container">
+      <Container>
+        <HashRouter>
           <NavbarComp />
-          <Route
-            path={process.env.PUBLIC_URL}
-            exact
-            component={withRouter(Homepage)}
-          />
-          <Route path={"/busyness"} exact component={withRouter(Viewpage)} />
-          <Route path={"/submit"} exact component={withRouter(Submitpage)} />
-        </div>
-      </Router>
+          <Route path={process.env.PUBLIC_URL} component={Homepage} />
+          <Route path={"/busyness"} component={Viewpage} />
+          <Route path={"/submit"} component={Submitpage} />
+        </HashRouter>
+      </Container>
     );
   }
 }
